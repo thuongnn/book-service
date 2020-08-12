1c0ada968908d2eba111efa0fa3bacfd0d9df207
Nguyễn Như Thưởng <thuongnn@ThuongNN.local>
2020-08-12 21:32:31 +0700

    update book api

--- src/api/home.go
+++ src/api/home.go
@@ -6,11 +6,7 @@ type HomeAPI struct {
 
 // default home page
 func (h *HomeAPI) Get() {
-	h.Data["json"] = "Welcome to Book API !\n" +
-		"GET /books \tList all books in DB\n" +
-		"POST /books \tCreate one book in DB\n" +
-		"GET /books/:id \tGet one book by id\n" +
-		"PUT /books/:id \tUpdate one book by id\n" +
-		"DELETE /books/:id \tDelete one book by id"
+	h.ServeXML()
+	h.Data["json"] = "Welcome to Book API !"
 	h.ServeJSON()
 }
